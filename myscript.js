8a3b0a9dca7d88d0c956ca2dd3f5035a3d48e4b3
jov316 <84162291+jov316@users.noreply.github.com>
2023-03-20 21:33:06 -0400

    inical commit

---
+++ myscript.js
@@ -0,0 +1,8 @@
+let title= document.getElementById("main-title")
+
+let content="Hola soy Jose Oval, Analista Quimico de Calidad"
+
+
+for( let i=0; i < length.title; i++){
+    title.innerText=content[i]
+}
